e3539a61c64cff120ae060354dc4836e2c8a045f
Laura Bigoni <laurabigoni.92@gmail.com>
2022-07-09 22:47:47 +0200

    update pwa

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+
+module.exports = {
+  pwa: {
+    workboxPluginMode: "GenerateSW",
+    workboxOptions: {
+      navigateFallback: "/index.html",
+    },
+  },
+};
